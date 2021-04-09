905ca5bc9aae03d2d7895d00e2888396f9c1fdf8
ua-lock <ggtbh@outlook.com>
2021-04-08 21:34:16 -0300

    feat(perf): ✨ ⚡️ detach from husky, install hooks manually

--- src/installHooks.js
+++ src/installHooks.js
@@ -1,38 +1,36 @@
 const path = require('path')
 const fs = require('fs')
-const { execSync } = require('child_process')
+const { spawnSync } = require('child_process')
 const { cyan } = require('chalk')
 
 const cz = require('./config/cz')
 const devmoji = require('./config/devmoji')
-const common = require('./config/common')
 const prepareCommitMessage = require('./config/prepare-commit-msg')
 
 const installHooks = (answers, packager) => {
   const pkg = (answers.packager || packager).toLowerCase()
+  const cmd = process.platform === 'win32' ? '.cmd' : ''
   const installCommand = pkg === 'yarn' ? 'add' : 'install'
 
   /* config files */
   const czConfigPath = path.resolve(process.cwd(), '.cz.config.js')
   const devmojiConfigPath = path.resolve(process.cwd(), 'devmoji.config.js')
   /* hook files */
-  const commonPath = path.resolve(process.cwd(), '.husky', 'common.sh')
-  const commitMsgHookPath = path.resolve(process.cwd(), '.husky', 'prepare-commit-msg')
+  const commitMsgHookPath = path.resolve(process.cwd(), '.git', 'hooks', 'prepare-commit-msg')
 
   try {
-    execSync(`${pkg} ${installCommand} -D devmoji husky`)
+    process.stdout.write('Installing Devmoji...')
+    const { status, stderr } = spawnSync(`${pkg}${cmd}`, [installCommand, '-D', 'devmoji'], { encoding: 'utf-8' })
+    if (status !== 0) throw Error(stderr)
+    process.stdout.clearLine()
+    process.stdout.write('Devmoji installed successfully.')
 
-    /* create husky folder if not already presents */
-    if (!fs.readdirSync(process.cwd()).includes('.husky')) {
-      fs.mkdirSync(path.resolve(process.cwd(), '.husky'))
-    }
-
+    process.stdout.write('Copying configuration files...')
     fs.writeFileSync(commitMsgHookPath, prepareCommitMessage(pkg === 'npm' ? 'npx' : pkg))
-    fs.writeFileSync(commonPath, common)
     fs.writeFileSync(czConfigPath, cz)
     fs.writeFileSync(devmojiConfigPath, devmoji)
+    process.stdout.clearLine()
 
-    execSync(`${pkg === 'npm' ? 'npx' : pkg} husky install`)
     console.log(cyan(`\n ✨ Commit hooks installed successfully!`))
   } catch (error) {
     console.error(error)
