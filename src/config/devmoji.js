e2b7e596fb23392f355009a95d98a78c70a7c2f6
ua-lock <ggtbh@outlook.com>
2021-04-10 23:08:55 -0300

    config: 🛠️ include `release` type

--- src/config/devmoji.js
+++ src/config/devmoji.js
@@ -1,12 +1,13 @@
 module.exports = `module.exports = {
-  types: ["docker", "revert", "gitignore", "ux", "types", "config"],
+  types: ["docker", "revert", "gitignore", "ux", "types", "config", "release"],
   devmoji: [
     { code: "docker", emoji: "whale" },
     { code: "revert", gitmoji: "wastebasket" },
     { code: "gitignore", gitmoji: "see_no_evil" },
     { code: "ux", gitmoji: "children_crossing" },
     { code: "test", emoji: "test_tube" },
-    { code: "config", emoji: "hammer_and_wrench" },
     { code: "types", emoji: "bookmark" },
+    { code: "config", emoji: "hammer_and_wrench" },
+    { code: "config", emoji: "hammer_and_wrench" },
   ],
 }`
